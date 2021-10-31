417aff1d3a40b7ecd207de547a2cf915bd09902e
Philippe Faist <phfaist@gmail.com>
2021-10-31 10:42:21 +0100

    update with new data

--- pretty_code_graph_assets/pretty_code_graph_data.js
+++ pretty_code_graph_assets/pretty_code_graph_data.js
@@ -3,28 +3,41 @@ pretty_code_graph_data = {
         "nodes": [
             {
                 "data": {
+                    "id": "c_oscillators",
+                    "label": "Oscillator code",
+                    "_description": "Encodes logical Hilbert space, finite- or infinite-dimensional, into Hilbert space of \\(\\ell^2\\)-normalizable functions on \\(\\mathbb{R}^n\\).",
+                    "_code_href": "/c/oscillators#ecc_oscillators",
+                    "_family_generation_level": 0
+                },
+                "position": {
+                    "x": -2,
+                    "y": -3
+                }
+            },
+            {
+                "data": {
                     "id": "c_qudits_into_oscillators",
                     "label": "Qudit-into-oscillator code",
                     "_description": "Encodes \\(K\\)-dimensional Hilbert space into Hilbert space of \\(\\ell^2\\)-normalizable functions on \\(\\mathbb{R}^n\\).",
                     "_code_href": "/c/qudits_into_oscillators#ecc_qudits_into_oscillators",
-                    "_family_generation_level": 0
+                    "_family_generation_level": 1
                 },
                 "position": {
-                    "x": 18,
-                    "y": -2
+                    "x": 7,
+                    "y": 130
                 }
             },
             {
                 "data": {
                     "id": "c_oscillators_into_oscillators",
-                    "label": "Oscillator code",
+                    "label": "Oscillator-into-oscillator code",
                     "_description": "Encodes Hilbert space of \\(\\ell^2\\)-normalizable functions on \\(\\mathbb{R}^k\\) into that on \\(\\mathbb{R}^n\\). Usually denoted as \\(((n,k))_{\\mathbb{R}}\\).",
                     "_code_href": "/c/oscillators_into_oscillators#ecc_oscillators_into_oscillators",
-                    "_family_generation_level": 0
+                    "_family_generation_level": 1
                 },
                 "position": {
-                    "x": 88,
-                    "y": 19
+                    "x": -24,
+                    "y": 130
                 }
             },
             {
@@ -35,11 +48,11 @@ pretty_code_graph_data = {
                         "An \\((n,2^k,d)\\) CSS code is usually denoted as \\([[n,k,d]]\\). Encoding is based on two binary linear codes, an \\([n,k_1,d_1]\\) code \\(C_1\\) and \\([n,k_2,d_2]\\) code \\(C_2\\) with \\(C_1^\\perp \\subse..."
                     ],
                     "_code_href": "/c/CSS#ecc_CSS",
-                    "_family_generation_level": 1
+                    "_family_generation_level": 2
                 },
                 "position": {
-                    "x": 215,
-                    "y": 71
+                    "x": 74,
+                    "y": 183
                 }
             },
             {
@@ -50,11 +63,11 @@ pretty_code_graph_data = {
                         "A family of stabilizer codes whose generators are few-body \\(X\\)-type Pauli strings (stars) and few-body \\(Z\\)-type Pauli strings on any surface plaquette operators on an arbitrary lattice on an ar..."
                     ],
                     "_code_href": "/c/toric#ecc_toric",
-                    "_family_generation_level": 1
+                    "_family_generation_level": 3
                 },
                 "position": {
-                    "x": 192,
-                    "y": 86
+                    "x": 112,
+                    "y": 304
                 }
             },
             {
@@ -66,7 +79,7 @@ pretty_code_graph_data = {
                     "_family_generation_level": 1
                 },
                 "position": {
-                    "x": 164,
+                    "x": 94,
                     "y": 75
                 }
             },
@@ -76,11 +89,11 @@ pretty_code_graph_data = {
                     "label": "Quantum repetition code",
                     "_description": "Encodes \\(1\\) qubit into \\(n\\) qubits according to \\(|0\\rangle\\to|0\\rangle^{\\otimes n}\\) and \\(|1\\rangle\\to|1\\rangle^{\\otimes n}\\).",
                     "_code_href": "/c/quantum_repetition#ecc_quantum_repetition",
-                    "_family_generation_level": 1
+                    "_family_generation_level": 3
                 },
                 "position": {
-                    "x": 188,
-                    "y": 108
+                    "x": 109,
+                    "y": 315
                 }
             },
             {
@@ -89,11 +102,24 @@ pretty_code_graph_data = {
                     "label": "Quantum low-density parity-check (LDPC) code",
                     "_description": "",
                     "_code_href": "/c/quantum_LDPC#ecc_quantum_LDPC",
-                    "_family_generation_level": 1
+                    "_family_generation_level": 2
                 },
                 "position": {
-                    "x": 217,
-                    "y": 93
+                    "x": 103,
+                    "y": 197
+                }
+            },
+            {
+                "data": {
+                    "id": "c_homological",
+                    "label": "Homological code",
+                    "_description": "Often called a topological code.",
+                    "_code_href": "/c/homological#ecc_homological",
+                    "_family_generation_level": 3
+                },
+                "position": {
+                    "x": 132,
+                    "y": 321
                 }
             },
             {
@@ -107,8 +133,23 @@ pretty_code_graph_data = {
                     "_family_generation_level": 0
                 },
                 "position": {
-                    "x": 217,
-                    "y": -9
+                    "x": 93,
+                    "y": 29
+                }
+            },
+            {
+                "data": {
+                    "id": "c_groups",
+                    "label": "Group code",
+                    "_description": [
+                        "Encodes a logical Hilbert space, finite- or infinite-dimensional, into the Hilbert space of \\(\\ell^2\\)-normalizable functions on a second-countable unimodular group. For \\(K\\)-dimensional logical s..."
+                    ],
+                    "_code_href": "/c/groups#ecc_groups",
+                    "_family_generation_level": 0
+                },
+                "position": {
+                    "x": 176,
+                    "y": -17
                 }
             },
             {
@@ -116,29 +157,14 @@ pretty_code_graph_data = {
                     "id": "c_group_gkp",
                     "label": "Group GKP codes",
                     "_description": [
-                        "Encodes finite-dimensional Hilbert space into the Hilbert space of \\(\\ell^2\\)-normalizable functions on a group \\(G\\). Construction is based on nested subgroups \\(H\\subset K \\subset G\\), where \\(H,..."
+                        "Group code whose construction is based on nested subgroups \\(H\\subset K \\subset G\\). Logical subspace is spanned by basis states that are equal superpositions of elements of cosets of \\(H\\) in \\(K\\..."
                     ],
                     "_code_href": "/c/group_gkp#ecc_group_gkp",
                     "_family_generation_level": 1
                 },
                 "position": {
-                    "x": 291,
-                    "y": 72
-                }
-            },
-            {
-                "data": {
-                    "id": "c_groups_into_groups",
-                    "label": "Group code",
-                    "_description": [
-                        "Encodes Hilbert space of \\(\\ell^2\\)-normalizable functions on a group \\(G_1\\) into that on a second-countable unimodular group \\(G_2\\). For finite \\(G_1\\) and \\(G_2=G^{\\times n}\\), can be denoted a..."
-                    ],
-                    "_code_href": "/c/groups_into_groups#ecc_groups_into_groups",
-                    "_family_generation_level": 0
-                },
-                "position": {
-                    "x": 260,
-                    "y": 13
+                    "x": 197,
+                    "y": 97
                 }
             },
             {
@@ -152,8 +178,8 @@ pretty_code_graph_data = {
                     "_family_generation_level": 2
                 },
                 "position": {
-                    "x": 261,
-                    "y": 185
+                    "x": 197,
+                    "y": 176
                 }
             },
             {
@@ -167,8 +193,8 @@ pretty_code_graph_data = {
                     "_family_generation_level": 0
                 },
                 "position": {
-                    "x": 420,
-                    "y": -11
+                    "x": 294,
+                    "y": 8
                 }
             },
             {
@@ -182,8 +208,8 @@ pretty_code_graph_data = {
                     "_family_generation_level": 1
                 },
                 "position": {
-                    "x": 472,
-                    "y": 77
+                    "x": 425,
+                    "y": 73
                 }
             },
             {
@@ -195,23 +221,8 @@ pretty_code_graph_data = {
                     "_family_generation_level": 0
                 },
                 "position": {
-                    "x": 525,
-                    "y": 30
-                }
-            },
-            {
-                "data": {
-                    "id": "c_linear",
-                    "label": "Linear code",
-                    "_description": [
-                        "An \\((n,2^k,d)_q\\) linear code is denoted as \\([n,k,d]_q\\). Its codewords form a linear subspace, i.e., for any codewords \\(x,y\\), \\(\\alpha x+ \\beta y\\) is also a codeword for any \\(q\\)-ary digits ..."
-                    ],
-                    "_code_href": "/c/linear#ecc_linear",
-                    "_family_generation_level": 1
-                },
-                "position": {
-                    "x": 601,
-                    "y": 99
+                    "x": 379,
+                    "y": 11
                 }
             },
             {
@@ -222,11 +233,11 @@ pretty_code_graph_data = {
                         "\\([n,k,n-k+1]_q\\) linear code. Let \\(\\{\\alpha_1,\\cdots,\\alpha_n\\}\\) be \\(n\\) distinct nonzero elements of \\(GF(q)\\) for prime \\(q>n\\). Encodes \\(\\mu=\\{\\mu_0,\\cdots,\\mu_{k-1}\\}\\) into \\(\\{f_\\mu(\\alp..."
                     ],
                     "_code_href": "/c/reed_solomon#ecc_reed_solomon",
-                    "_family_generation_level": 1
+                    "_family_generation_level": 2
                 },
                 "position": {
-                    "x": 564,
-                    "y": 74
+                    "x": 502,
+                    "y": 185
                 }
             },
             {
@@ -240,24 +251,48 @@ pretty_code_graph_data = {
                     "_family_generation_level": 0
                 },
                 "position": {
-                    "x": 591,
-                    "y": -14
+                    "x": 486,
+                    "y": 28
+                }
+            },
+            {
+                "data": {
+                    "id": "c_q-ary_linear",
+                    "label": "\\(q\\)-ary linear code",
+                    "_description": [
+                        "An \\((n,2^k,d)_q\\) linear code is denoted as \\([n,k,d]_q\\). Its codewords form a linear subspace, i.e., for any codewords \\(x,y\\), \\(\\alpha x+ \\beta y\\) is also a codeword for any \\(q\\)-ary digits ..."
+                    ],
+                    "_code_href": "/c/q-ary_linear#ecc_q-ary_linear",
+                    "_family_generation_level": 1
+                },
+                "position": {
+                    "x": 476,
+                    "y": 103
                 }
             }
         ],
         "edges": [
             {
                 "data": {
-                    "id": "parent_CSS_qubits_into_qubits_0",
+                    "id": "parent_qudits_into_oscillators_oscillators_0",
                     "label": "parent",
                     "_rel_type": "parent",
-                    "source": "c_CSS",
-                    "target": "c_qubits_into_qubits"
+                    "source": "c_qudits_into_oscillators",
+                    "target": "c_oscillators"
                 }
             },
             {
                 "data": {
-                    "id": "parent_CSS_stabilizer_1",
+                    "id": "parent_oscillators_into_oscillators_oscillators_1",
+                    "label": "parent",
+                    "_rel_type": "parent",
+                    "source": "c_oscillators_into_oscillators",
+                    "target": "c_oscillators"
+                }
+            },
+            {
+                "data": {
+                    "id": "parent_CSS_stabilizer_2",
                     "label": "parent",
                     "_rel_type": "parent",
                     "source": "c_CSS",
@@ -275,15 +310,6 @@ pretty_code_graph_data = {
             },
             {
                 "data": {
-                    "id": "parent_toric_qubits_into_qubits_2",
-                    "label": "parent",
-                    "_rel_type": "parent",
-                    "source": "c_toric",
-                    "target": "c_qubits_into_qubits"
-                }
-            },
-            {
-                "data": {
                     "id": "parent_toric_CSS_3",
                     "label": "parent",
                     "_rel_type": "parent",
@@ -293,11 +319,11 @@ pretty_code_graph_data = {
             },
             {
                 "data": {
-                    "id": "parent_toric_stabilizer_4",
+                    "id": "parent_toric_homological_4",
                     "label": "parent",
                     "_rel_type": "parent",
                     "source": "c_toric",
-                    "target": "c_stabilizer"
+                    "target": "c_homological"
                 }
             },
             {
@@ -311,47 +337,38 @@ pretty_code_graph_data = {
             },
             {
                 "data": {
-                    "id": "parent_quantum_repetition_qubits_into_qubits_6",
+                    "id": "parent_quantum_repetition_CSS_6",
                     "label": "parent",
                     "_rel_type": "parent",
                     "source": "c_quantum_repetition",
-                    "target": "c_qubits_into_qubits"
+                    "target": "c_CSS"
                 }
             },
             {
                 "data": {
-                    "id": "parent_quantum_repetition_stabilizer_7",
+                    "id": "parent_quantum_LDPC_stabilizer_7",
                     "label": "parent",
                     "_rel_type": "parent",
-                    "source": "c_quantum_repetition",
+                    "source": "c_quantum_LDPC",
                     "target": "c_stabilizer"
                 }
             },
             {
                 "data": {
-                    "id": "cousin_quantum_repetition_stabilizer_1",
-                    "label": "cousin",
-                    "_rel_type": "cousin",
-                    "source": "c_quantum_repetition",
-                    "target": "c_binary_linear"
-                }
-            },
-            {
-                "data": {
-                    "id": "parent_quantum_LDPC_qubits_into_qubits_8",
+                    "id": "parent_homological_CSS_8",
                     "label": "parent",
                     "_rel_type": "parent",
-                    "source": "c_quantum_LDPC",
-                    "target": "c_qubits_into_qubits"
+                    "source": "c_homological",
+                    "target": "c_CSS"
                 }
             },
             {
                 "data": {
-                    "id": "parent_group_gkp_groups_into_groups_9",
+                    "id": "parent_group_gkp_groups_9",
                     "label": "parent",
                     "_rel_type": "parent",
                     "source": "c_group_gkp",
-                    "target": "c_groups_into_groups"
+                    "target": "c_groups"
                 }
             },
             {
@@ -374,19 +391,19 @@ pretty_code_graph_data = {
             },
             {
                 "data": {
-                    "id": "parent_linear_q-ary_digits_into_q-ary_digits_12",
+                    "id": "parent_reed_solomon_q-ary_linear_12",
                     "label": "parent",
                     "_rel_type": "parent",
-                    "source": "c_linear",
-                    "target": "c_q-ary_digits_into_q-ary_digits"
+                    "source": "c_reed_solomon",
+                    "target": "c_q-ary_linear"
                 }
             },
             {
                 "data": {
-                    "id": "parent_reed_solomon_q-ary_digits_into_q-ary_digits_13",
+                    "id": "parent_q-ary_linear_q-ary_digits_into_q-ary_digits_13",
                     "label": "parent",
                     "_rel_type": "parent",
-                    "source": "c_reed_solomon",
+                    "source": "c_q-ary_linear",
                     "target": "c_q-ary_digits_into_q-ary_digits"
                 }
             }
